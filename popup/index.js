e316dfc774b8bc1355632ae0b8a27e7b65739473
webpatch <webpatch@sina.com>
2017-11-22 17:38:04 +0800

    update

--- popup/index.js
+++ popup/index.js
@@ -1,11 +1,17 @@
-browser.tabs.query({ active: true }).then(tabs => {
-  console.log(tabs[0].url);
+function create(url) {
   new QRCode("qrcode", {
-    text: tabs[0].url,
+    text: url,
     width: 200,
     height: 200,
     colorDark: "#000000",
     colorLight: "#ffffff",
-    correctLevel: QRCode.CorrectLevel.H
+    correctLevel: QRCode.CorrectLevel.L
   });
-});
+}
+
+if (typeof browser !== 'undefined') {
+  browser.tabs.query({ active: true }).then(tabs => create(tabs[0].url));
+} else if (typeof chrome !== 'undefined') {
+  chrome.tabs.query({ active: true }, tabs => create(tabs[0].url));
+}
+
